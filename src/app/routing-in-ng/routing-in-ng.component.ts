7530e65d27a9e2cc080259a1d590cafc75906473
ashwani-bhadani <ashwani.bhadani.3289@gmail.com>
2025-04-24 22:13:59 +0530

    routing in ng

--- src/app/routing-in-ng/routing-in-ng.component.ts
+++ src/app/routing-in-ng/routing-in-ng.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-routing-in-ng',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterOutlet],
   templateUrl: './routing-in-ng.component.html',
   styleUrl: './routing-in-ng.component.scss'
 })
 export class RoutingInNgComponent {
-  //routing:
+  //routing: import router link & router outlet
   //import & register route file, make links for open pages with routes, register a component & use it as a component
+
+  //header component in routing, active class, feature of router link active options, adding homepage to route path
 
 }
