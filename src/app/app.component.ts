0dfb09363ab5f3e9525b10231598940efc070de0
ashwani-bhadani <ashwani.bhadani.3289@gmail.com>
2025-06-29 11:26:34 +0530

    basic template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,101 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { ProfileComponent } from './profile/profile.component';
-import { DatatypesComponent } from './datatypes/datatypes.component';
-import { CounterComponent } from './counter/counter.component';
-import { EventsInNgComponent } from './events-in-ng/events-in-ng.component';
-import { InputsInNgComponent } from './inputs-in-ng/inputs-in-ng.component';
-import { IfelseInNgComponent } from './ifelse-in-ng/ifelse-in-ng.component';
-import { SignalsInNgComponent } from './signals-in-ng/signals-in-ng.component';
-import { EffectsInNgComponent } from './effects-in-ng/effects-in-ng.component';
-import { ForLoopInNgComponent } from './for-loop-in-ng/for-loop-in-ng.component';
-import { TwoWayBindingInNgComponent } from "./two-way-binding-in-ng/two-way-binding-in-ng.component";
-import { ToDoListComponent } from "./to-do-list/to-do-list.component";
-import { DynamicStylingComponent } from "./dynamic-styling/dynamic-styling.component";
-import { DirectivesInNgComponent } from "./directives-in-ng/directives-in-ng.component";
-import { RoutingInNgComponent } from "./routing-in-ng/routing-in-ng.component";
-import { BasicReactiveFormsComponent } from "./basic-reactive-forms/basic-reactive-forms.component";
-import { ReactiveFormGroupingComponent } from './reactive-form-grouping/reactive-form-grouping.component';
-import { TemplateDrivenFormsComponent } from './template-driven-forms/template-driven-forms.component';
-import { PassDataFromParentToChildComponent } from "./pass-data-from-parent-to-child/pass-data-from-parent-to-child.component";
-import { ReusingComponent } from "./reusing/reusing.component";
-import { PipesInNgComponent } from "./pipes-in-ng/pipes-in-ng.component";
-import { NgIf } from '@angular/common';
-import { AmountServiceService } from './services/amount-service.service';
-import { ApiCallingService } from './services/api-calling.service';
-import { FormsModule, NgForm } from '@angular/forms';
-import { Post } from './interfaces/post';
-import { LazyLoadingRouteComponent } from './lazy-loading-route/lazy-loading-route.component';
-
+import { BasicReactiveFormsComponent } from './basic-reactive-forms/basic-reactive-forms.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  // imports: [LoginComponent,CounterComponent, ProfileComponent, DatatypesComponent,
-  //  EventsInNgComponent,InputsInNgComponent, IfelseInNgComponent, SignalsInNgComponent, EffectsInNgComponent,
-  // ForLoopInNgComponent,TwoWayBindingInNgComponent, ToDoListComponent, DynamicStylingComponent, DirectivesInNgComponent,
-  // RoutingInNgComponent, BasicReactiveFormsComponent,ReactiveFormGroupingComponent, TemplateDrivenFormsComponent,PassDataFromParentToChildComponent
-  // ReusingComponent, PipesInNgComponent, NgIf
-  // ],
-  imports: [FormsModule, LazyLoadingRouteComponent, RouterOutlet],
+  imports: [BasicReactiveFormsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-
-  postsAll:any
-  // !! it is very imp to declare data type using interfaces in angular & use them instead
-  //example calling APIs 
-  constructor(private apiServie:ApiCallingService){}
-  
-  ngOnInit(){ //ek provider be add karna padega in app.config.ts, htthProvider otherise error dega
-  //   R3InjectorError(Environment Injector)[_ApiCallingService -> _HttpClient -> _HttpClient]: 
-  // NullInjectorError: No provider for _HttpClient!
-  
-    /* "Subscribe to the observable returned by the HTTP call to start the request and asynchronously
-     handle the emitted response data. In Angular, HTTP methods like GET return cold observables, 
-     which execute only upon subscription."
-    subscribe() ke through response ko asynchronously handle karte hain (kyunki HTTP responses late aate hain).
-     */
-    this.apiServie.getJsonPlaceholderPosts().subscribe(
-      (data:any)=>{
-        console.log(data)
-        this.postsAll= data
-      }
-      
-    )//subscribe takes a callback function
-  }
-
-  //method to add a post by POST API via form & call method in api-calling service
-  addPostByUserId(post:Post){
-    this.apiServie.savePost(post).subscribe(
-      (data:Post) =>{
-        console.log(data)
-        // this.postsAll.s
-      }
-    )
-  }
-
-  
-  //example for lifecycle methods in angular
-  // counter=0
-  // increaseCounter(){
-  //   this.counter+=1
-  // }
-  
-  //example for sending data from parent -> child & vice versa
-  // title = 'ng-19-tutorials'; //you can also send property from parent to child
-  // //to take data from html select box
-  // onSelectUser(user:string){
-  //   this.title= ""
-  // }
-
-  // //reusing data properties defined in resusingComponent 
-  // reuseUsers(users:string[]){
-  //   console.log(users) //to pass this function to child component use banana braces in html
-  // }
-
-  // name = 'ashwani'; //these properties available to app.component.html as defined in templateUrl
-  // user1="peter";
-  // user2="sam"
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,28 +1,4 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { ContactComponent } from './contact/contact.component';
-import { HomeComponent } from './home/home.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { ProfileComponent } from './profile/profile.component';
-import { UserComponent } from './user/user.component';
-import { LazyLoadingRouteComponent } from './lazy-loading-route/lazy-loading-route.component';
-
 //register all the components here by creating route objects
 export const routes: Routes = [
-    {path:'login', component: LoginComponent},
-    {path:'contact', component: ContactComponent},
-    // {path:'about', component: AboutComponent},
-    {path:'profile', component: ProfileComponent},
-    // {path:'lazy-load', component: LazyLoadingRouteComponent},//lazy loading route/ component
-    //now this component will be lazily loaded when it is needed, i.e, when the route is called, apply this to heavy pages, less used pages
-    {path:'lazy-load', loadComponent:()=>import('./lazy-loading-route/lazy-loading-route.component')
-        .then((c)=>c.LazyLoadingRouteComponent)},
-
-    {path:'user/:id/:name', component:UserComponent}, //what key u put here will be used to get data & place in url
-    {path: '', component: HomeComponent}, //home will be on base path, so router link active
-    //  will also apply to base as it is always active, so use router link active option
-    
-    // >>>> also ng from top starts to match the routes, so if you keep wildcard at start, other routes will fail <<<<<<
-    {path: '**', component:PageNotFoundComponent} //this is called wild card matching,
-    //  all pages except mentioned will have this component shown
 ];
